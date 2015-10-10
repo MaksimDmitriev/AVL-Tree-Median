6d7aa34e68f4d1416ab7e79bfec4ae9a4ba52ad2
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-10-11 01:20:19 +0400

    Preparing for revision 2

--- src/com/avl/AvlTree.java
+++ src/com/avl/AvlTree.java
@@ -1,99 +1,36 @@
 package com.avl;
 
 
-public class AvlTree {
+public class AvlTree<T extends Comparable<T>> {
 
-	private Node root;
-	private int size;
-	private int oldSize;
-	private int leftSubtreeSize;
-	private int rightSubtreeSize;
+    private Node<T> root;
+    private int size;
 
-	public AvlTree(int... keys) {
-		if (keys == null) {
-			return;
-		}
-		insert(keys);
-	}
-
-	private Node insert(Node parent, int key) {
-		if (parent == null) {
-			size++;
-			return new Node(key);
-		}
-		if (key < parent.key) {
-			parent.left = insert(parent.left, key);
-			if (size > oldSize) {
-				leftSubtreeSize++;
-				oldSize = size;
-			}
-		} else if (key > parent.key) {
-			parent.right = insert(parent.right, key);
-			if (size > oldSize) {
-				rightSubtreeSize++;
-				oldSize = size;
-			}
-		}
-		return balance(parent);
-	}
-
-	private Node balance(Node p) {
-		fixHeight(p);
-		if (bfactor(p) == 2) {
-			if (bfactor(p.right) < 0) {
-				p.right = rotateRight(p.right);
-			}
-			return rotateLeft(p);
-		}
-		if (bfactor(p) == -2) {
-			if (bfactor(p.left) > 0) {
-				p.left = rotateLeft(p.left);
-			}
-			return rotateRight(p);
-		}
-		return p;
-	}
-
-	private Node rotateRight(Node p) {
-		Node q = p.left;
-		p.left = q.right;
-		q.right = p;
-		fixHeight(p);
-		fixHeight(q);
-		return q;
-	}
-
-	private Node rotateLeft(Node q) {
-		Node p = q.right;
-		q.right = p.left;
-		p.left = q;
-		fixHeight(q);
-		fixHeight(p);
-		return p;
-	}
-
-	private int height(Node p) {
-		return p == null ? 0 : p.height;
-	}
-
-	private int bfactor(Node p) {
-		return height(p.right) - height(p.left);
-	}
-
-	public double getMedian() {
-		if (root == null) {
-			throw new IllegalStateException("Can't get a median if the tree is empty");
-		}
-		if (size % 2 == 0) {
-            int b = getNode(root, size / 2 - 1).key;
-            int a = getNode(root, size / 2).key;
-            return 0.5 * (a + b);
-        } else {
-            return getNode(root, size / 2).key;
+    public AvlTree(T... keys) {
+        if (keys == null) {
+        	return;
         }
-	}
-	
-	/**
+        insert(keys);
+    }
+
+    /**
+     * Computes the number of child nodes in the left subtree of {@code node}.
+     * Runs in constant time.
+     * 
+     * @param  node the node whose left subtree size to compute.
+     * @return the number of child nodes in the left subtree.
+     */
+    private int getLeftSubtreeSize(Node<T> node) {
+        int tmp = node.childCount;
+
+        if (node.right != null) {
+            tmp -= (node.right.childCount + 1);
+        }
+
+        return tmp;
+    }
+
+    /**
      * Returns the value of {@code index}th (in order) entry. Runs in 
      * logarithmic time.
      * 
@@ -101,39 +38,139 @@ public class AvlTree {
      * @param index the index of the entry whose value to get.
      * @return the value of {@code index}th value.
      */
-    private Node getNode(Node root, int index) {
-        Node current = root;
-        // TODO: 
-        return null;
+    private Node<T> getNode(Node<T> root, int index) {
+        Node<T> current = root;
+
+        for (;;) {
+            int leftSubtreeSize = getLeftSubtreeSize(current);
+
+            if (index == leftSubtreeSize) {
+                return current;
+            }
+
+            if (index > leftSubtreeSize) {
+                index -= (leftSubtreeSize + 1);
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
     }
 
-	private void fixHeight(Node p) {
-		int hl = height(p.left);
-		int hr = height(p.right);
-		p.height = (hl > hr ? hl : hr) + 1;
-	}
+    /**
+     * Computes the median of this tree. Makes at most two calls to logarithmic 
+     * time methods.
+     * 
+     * @return the median of this tree.
+     */
+    public double getMedian() {
+        if (size == 0) {
+            throw new IllegalStateException(
+                    "Asking for median from an empty tree.");
+        }
 
-	public void insert(int... keys) {
-		for (int key : keys) {
-			root = insert(root, key);
-		}
-	}
+        if (size % 2 == 0) {
+            T b = getNode(root, size / 2 - 1).key;
+            T a = getNode(root, size / 2).key;
+            return 0.5 * (a + b);
+        } else {
+            return getNode(root, size / 2).key;
+        }
+    }
 
-	private static class Node {
+    private Node<T> insert(Node<T> parent, T key) {
+        if (parent == null) {
+            ++size;
+            return new Node<T>(key);
+        }
+        
+        if (key.compareTo(parent.key) < 0) {
+            parent.left = insert(parent.left, key);
+        } else {
+            parent.right = insert(parent.right, key);
+        }
 
-		private Node left;
-		private Node right;
-		private final int key;
-		private int height;
+        return balance(parent);
+    }
 
-		private Node(int value) {
-			key = value;
-			height = 1;
-		}
+    private Node<T> balance(Node<T> p) {
+        fixHeightAndChildCount(p);
+        if (bfactor(p) == 2) {
+            if (bfactor(p.right) < 0) {
+                p.right = rotateRight(p.right);
+            }
+            return rotateLeft(p);
+        }
+        if (bfactor(p) == -2) {
+            if (bfactor(p.left) > 0) {
+                p.left = rotateLeft(p.left);
+            }
+            return rotateRight(p);
+        }
+        return p;
+    }
 
-		@Override
-		public String toString() {
-			return Integer.toString(key);
-		}
-	}
+    private Node<T> rotateRight(Node<T> p) {
+        Node<T> q = p.left;
+        p.left = q.right;
+        q.right = p;
+        fixHeightAndChildCount(p);
+        fixHeightAndChildCount(q);
+        return q;
+    }
+
+    private Node<T> rotateLeft(Node<T> q) {
+        Node<T> p = q.right;
+        q.right = p.left;
+        p.left = q;
+        fixHeightAndChildCount(q);
+        fixHeightAndChildCount(p);
+        return p;
+    }
+
+    private int height(Node<T> p) {
+        return p == null ? 0 : p.height;
+    }
+
+    private int bfactor(Node<T> p) {
+        return height(p.right) - height(p.left);
+    }
+
+    private void fixHeightAndChildCount(Node<T> p) {
+        int hl = height(p.left);
+        int hr = height(p.right);
+        p.height = (hl > hr ? hl : hr) + 1;
+        p.childCount = 0;
+        if (p.left != null) {
+            p.childCount = p.left.childCount + 1;
+        }
+        if (p.right != null) {
+            p.childCount += p.right.childCount + 1;
+        }
+    }
+
+    public void insert(T... keys) {
+        for (T key : keys) {
+            root = insert(root, key);
+        }
+    }
+
+    private static final class Node<T> {
+
+        private Node<T> left;
+        private Node<T> right;
+        private final T key;
+        private int height;
+        private int childCount;
+
+        private Node(T value) {
+            key = value;
+            height = 1;
+        }
+
+        @Override
+        public String toString() {
+            return key == null ? "" : key.toString();
+        }
+    }
 }
