1efcd0645d7a72cf97aed523569204cfe21fff9a
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-10-12 21:49:27 +0400

    Get ready for revision 2

--- src/com/client/Main.java
+++ src/com/client/Main.java
@@ -1,26 +1,8 @@
 package com.client;
 
-import com.avl.AvlTree;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-	}
-	
-	static class A<T extends Number> {}
-	
-	private static void runWithAvl() {
-		AvlTree avlTree = new AvlTree();
-		for (int i = 0; i < 10000000; i++) {
-			avlTree.insert(i);
-		}
-		long start = System.currentTimeMillis();
-		avlTree.getMedian();
-		printDuration("AvlTree ", start, System.currentTimeMillis());
-	}
-	
-	private static void printDuration(String operation, long start, long end) {
-		System.out.println(operation + (end - start) / 1000.0);
+		// Client code goes here
 	}
 }
